bd9f50308ee53d9097431badbaefdd65227845d0
alimed123 <92314175+alimed123@users.noreply.github.com>
2022-03-04 22:25:01 +0100

    Add files via upload

---
+++ instagram.py
@@ -0,0 +1,10 @@
+read me!
+please make sure to install these libraries by using cmd
+pip install pandas
+pip install selenium
+pip install DateTime
+pip install openpyxl
+
+make sure to put geckodriver on the program folder
+geckodriver:https://github.com/mozilla/geckodriver/releases/download/v0.30.0/geckodriver-v0.30.0-win64.zip
+make sure to install firefox last update.
